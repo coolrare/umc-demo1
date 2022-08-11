334739232968ab6428b8bad900dc5fe7c505d856
Will <doggy.huang@gmail.com>
2022-08-11 15:09:24 +0800

    示範內嵌繫結、屬性繫結、事件繫結、雙向繫結

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'umc-demo1';
+  keyword = 'Test';
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  title = 'umc-demo1';
+  sitelogo = '/assets/images/logo.png';
+
   constructor() { }
 
   ngOnInit(): void {
+    // setTimeout(() => {
+    //   this.title = 'The Will Will Web';
+    // }, 2000);
+  }
+
+  changeTitle($event: PointerEvent | MouseEvent) {
+    console.log($event);
+    if ($event.ctrlKey) {
+      this.title = 'The Will Will Web';
+    }
   }
 
 }
